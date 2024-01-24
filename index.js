883b002a8f1a76fe531633f5c7bb08e22dce86d4
Agnieszka Magiera <agnieszka_magiera@wp.pl>
2024-01-24 17:04:08 +0100

    Dodanie klas do input i zainicjalowanie zmiennych

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ const resCopy = document.querySelector('.copy-result');
 const copyIcn = document.querySelector('.material-symbols-outlined');
 const charLng = document.querySelector('.char-length');
 const suw = document.querySelector('.suw');
+const upCase = document.querySelector('.upCase');
+const lowCase = document.querySelector('.lowCase');
+const numbers = document.querySelector('.numbers');
+const chars = document.querySelector('.chars');
