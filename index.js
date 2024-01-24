ef5da06e1c9e03839b0e070c8a78ead91a5182fd
Agnieszka Magiera <agnieszka_magiera@wp.pl>
2024-01-24 16:58:59 +0100

    Zainicjowanie zmiennych

---
+++ index.js
@@ -0,0 +1,4 @@
+const resCopy = document.querySelector('.copy-result');
+const copyIcn = document.querySelector('.material-symbols-outlined');
+const charLng = document.querySelector('.char-length');
+const suw = document.querySelector('.suw');
