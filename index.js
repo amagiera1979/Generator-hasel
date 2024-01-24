d454e4b6fddb8291e1fa49e6b42235af279920a3
Agnieszka Magiera <agnieszka_magiera@wp.pl>
2024-01-24 18:48:43 +0100

    Funkcja suwaka onDragSuw, poprawki w .css i .html

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ const upCase = document.querySelector('.upCase');
 const lowCase = document.querySelector('.lowCase');
 const numbers = document.querySelector('.numbers');
 const chars = document.querySelector('.chars');
+
+
+
+function onDragSuw() {
+console.log(suw.value)
+let suwVal = suw.value;
+charLng.innerText = suwVal;
+}
+
+suw.addEventListener('click', onDragSuw);
