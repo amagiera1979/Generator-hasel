5e96eef770d893654aa42c334a1a32b6a72a48e6
Agnieszka Magiera <agnieszka_magiera@wp.pl>
2024-01-25 16:14:05 +0100

    Funkcja fillTable czyli wypisanie wszystkich potrzebnych znaków

--- index.js
+++ index.js
@@ -6,13 +6,34 @@ const upCase = document.querySelector('.upCase');
 const lowCase = document.querySelector('.lowCase');
 const numbers = document.querySelector('.numbers');
 const chars = document.querySelector('.chars');
+const password =[];
 
+function fillTable(){
+for(let i=32; i<=126; i++){
+    password.push(String.fromCharCode(i));
+}
+
+   console.log(password)
+}
+
+function addUpCase() {
+    const upC=/[A-Z]/;
+    const up = password.filter([A-Z]);
+    console.log(up)
+
+
+// upCase.value==='on' ? password.push()
+}
+
+upCase.addEventListener('click', addUpCase)
 
 
 function onDragSuw() {
-console.log(suw.value)
+// console.log(suw.value)
 let suwVal = suw.value;
 charLng.innerText = suwVal;
 }
 
 suw.addEventListener('click', onDragSuw);
+
+window.addEventListener('load', fillTable);
