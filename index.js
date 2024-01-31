bbda6b29308100db2cb460f9f8047505d3549e5a
Agnieszka Magiera <agnieszka_magiera@wp.pl>
2024-01-31 19:25:43 +0100

    Zakomentowanie programu pobierajacego znaki z tabeli ASCII

--- index.js
+++ index.js
@@ -1,3 +1,159 @@
+// OBSŁUGA GENERATORA HASEŁ Z POBIERANIEM ZNAKÓW Z TABELI ASCII
+
+// const resCopy = document.querySelector('.copy-result');
+// const charLng = document.querySelector('.char-length');
+// const suw = document.querySelector('.suw');
+// const button = document.querySelector('button');
+// const strBar = document.querySelector('.str-bar');
+// const bar = document.querySelectorAll('.bar');
+// const input = document.querySelectorAll('#input-check');
+// let inputChecked = 0;
+// let password = [];
+
+// // Funkcja zmieniająca klasę w 'pasku postępu' mocy
+// function changeBar(inputChecked) {
+//   inputChecked > 10
+//     ? (bar[0].className = 'bar-on')
+//     : (bar[0].className = 'bar');
+
+//   inputChecked > 30
+//     ? (bar[1].className = 'bar-on')
+//     : (bar[1].className = 'bar');
+
+//   inputChecked > 50
+//     ? (bar[2].className = 'bar-on')
+//     : (bar[2].className = 'bar');
+
+//   inputChecked > 70
+//     ? (bar[3].className = 'bar-on')
+//     : (bar[3].className = 'bar');
+// }
+
+// // Funkcja sprawdzania mocy hasła
+// function strengthPassword(inputChecked) {
+//   if (inputChecked <= 36) {
+//     strBar.innerHTML = 'słaba';
+//   } else if (inputChecked >= 37 && inputChecked <= 51) {
+//     strBar.innerHTML = 'średnia';
+//   } else if (inputChecked >= 52 && inputChecked <= 69) {
+//     strBar.innerHTML = 'mocna';
+//   } else if (inputChecked >= 70) {
+//     strBar.innerHTML = 'bardzo mocna';
+//   }
+//   changeBar(inputChecked);
+// }
+
+// input.forEach(i => {
+//   i.addEventListener('click', e => {
+//     // Wstawianie wielkich liter
+//     if (i.className === 'upCase') {
+//       for (let a = 65; a <= 90; a++) {
+//         // Sprawdzanie wartości pola checked
+//         if (e.target.checked) {
+//           password.push(String.fromCharCode(a));
+//           inputChecked++;
+//         } else {
+//           password.pop(String.fromCharCode(a));
+//           inputChecked--;
+//         }
+//       }
+//     } else if (i.className === 'lowCase') {
+//       // Wstawianie małych liter
+//       for (let a = 97; a <= 122; a++) {
+//         // Sprawdzanie wartości pola checked
+//         if (e.target.checked) {
+//           password.push(String.fromCharCode(a));
+//           inputChecked++;
+//         } else {
+//           password.pop(String.fromCharCode(a));
+//           inputChecked--;
+//         }
+//       }
+//     } else if (i.className === 'numbers') {
+//       // Wstawianie cyfr
+//       for (let a = 48; a <= 57; a++) {
+//         // Sprawdzanie wartości pola checked
+//         if (e.target.checked) {
+//           password.push(String.fromCharCode(a));
+//           inputChecked++;
+//         } else {
+//           password.pop(String.fromCharCode(a));
+//           inputChecked--;
+//         }
+//       }
+//     } else if (i.className === 'chars') {
+//       // Dodawanie znaków
+//       for (let a = 32; a <= 126; a++) {
+//         // Sprawdzanie wartości pola checked
+//         if (e.target.checked) {
+//           if (a >= 32 && a <= 47) {
+//             password.push(String.fromCharCode(a));
+//             inputChecked++;
+//           } else if (a >= 58 && a <= 64) {
+//             password.push(String.fromCharCode(a));
+//             inputChecked++;
+//           } else if (a >= 91 && a <= 96) {
+//             password.push(String.fromCharCode(a));
+//             inputChecked++;
+//           } else if (a >= 123 && a <= 126) {
+//             password.push(String.fromCharCode(a));
+//             inputChecked++;
+//           }
+//         } else {
+//           if (a >= 32 && a <= 47) {
+//             password.pop(String.fromCharCode(a));
+//             inputChecked--;
+//           } else if (a >= 58 && a <= 64) {
+//             password.pop(String.fromCharCode(a));
+//             inputChecked--;
+//           } else if (a >= 91 && a <= 96) {
+//             password.pop(String.fromCharCode(a));
+//             inputChecked--;
+//           } else if (a >= 123 && a <= 126) {
+//             password.pop(String.fromCharCode(a));
+//             inputChecked--;
+//           }
+//         }
+//       }
+//     }
+//     strengthPassword(inputChecked);
+
+//     takePassword(password);
+//   });
+// });
+
+// // Generowanie hasła
+// function takePassword(password) {
+//   button.addEventListener('click', e => {
+//     // Z ilu znaków składa się hasło
+//     let num = charLng.innerHTML;
+//     // Zamiana tablicy na string, aby użyć charAt. Bez join() dokłada przecinki
+//     let str = password.join('').toString();
+//     let arr = [];
+
+//     if (num === '') {
+//       alert('Zaznacz długość hasła');
+//     }
+//     // Losowanie znaków
+//     for (let i = 0; i < num; i++) {
+//       const res = Math.floor(Math.random() * password.length);
+//       arr.push(str.charAt(res));
+//     }
+//     // Wyświetlenie hasła. Bez join() dodaje przecinki
+//     resCopy.innerHTML = arr.join('');
+//   });
+// }
+
+// function onDragSuw(e) {
+//   // console.log(e.target.value)
+//   let suwVal = e.target.value;
+//   charLng.innerText = suwVal;
+// }
+
+// suw.addEventListener('click', onDragSuw);
+
+// !!!  OBSŁUGA GENERATORA HASEŁ Z GOTOWYMI CIĄGAMI ZNAKÓW
+
 const resCopy = document.querySelector('.copy-result');
 // const copyIcn = document.querySelector('.material-symbols-outlined');
 const charLng = document.querySelector('.char-length');
@@ -12,169 +168,6 @@ const bar = document.querySelectorAll('.bar');
 const input = document.querySelectorAll('#input-check');
 let inputChecked = 0;
 let password = [];
-
-// Funkcja zmieniająca klasę w 'pasku postępu' mocy
-function changeBar(inputChecked) {
-  inputChecked > 10
-    ? (bar[0].className = 'bar-on')
-    : (bar[0].className = 'bar');
-
-  inputChecked > 30
-    ? (bar[1].className = 'bar-on')
-    : (bar[1].className = 'bar');
-
-  inputChecked > 50
-    ? (bar[2].className = 'bar-on')
-    : (bar[2].className = 'bar');
-
-  inputChecked > 70
-    ? (bar[3].className = 'bar-on')
-    : (bar[3].className = 'bar');
-}
-
-// Funkcja sprawdzania mocy hasła
-function strengthPassword(inputChecked) {
-  if (inputChecked <= 36) {
-    strBar.innerHTML = 'słaba';
-  } else if (inputChecked >= 37 && inputChecked <= 51) {
-    strBar.innerHTML = 'średnia';
-  } else if (inputChecked >= 52 && inputChecked <= 69) {
-    strBar.innerHTML = 'mocna';
-  } else if (inputChecked >= 70) {
-    strBar.innerHTML = 'bardzo mocna';
-  }
-  changeBar(inputChecked);
-}
-
-input.forEach(i => {
-  i.addEventListener('click', e => {
-    // Wstawianie wielkich liter
-    if (i.className === 'upCase') {
-      for (let a = 65; a <= 90; a++) {
-        // Sprawdzanie wartości pola checked
-        if (e.target.checked) {
-          password.push(String.fromCharCode(a));
-          inputChecked++;
-        } else {
-          password.pop(String.fromCharCode(a));
-          inputChecked--;
-        }
-      }
-    } else if (i.className === 'lowCase') {
-      // Wstawianie małych liter
-      for (let a = 97; a <= 122; a++) {
-        // Sprawdzanie wartości pola checked
-        if (e.target.checked) {
-          password.push(String.fromCharCode(a));
-          inputChecked++;
-        } else {
-          password.pop(String.fromCharCode(a));
-          inputChecked--;
-        }
-      }
-    } else if (i.className === 'numbers') {
-      // Wstawianie cyfr
-      for (let a = 48; a <= 57; a++) {
-        // Sprawdzanie wartości pola checked
-        if (e.target.checked) {
-          password.push(String.fromCharCode(a));
-          inputChecked++;
-        } else {
-          password.pop(String.fromCharCode(a));
-          inputChecked--;
-        }
-      }
-    } else if (i.className === 'chars') {
-      // Dodawanie znaków
-      for (let a = 32; a <= 126; a++) {
-        // Sprawdzanie wartości pola checked
-        if (e.target.checked) {
-          if (a >= 32 && a <= 47) {
-            password.push(String.fromCharCode(a));
-            inputChecked++;
-          } else if (a >= 58 && a <= 64) {
-            password.push(String.fromCharCode(a));
-            inputChecked++;
-          } else if (a >= 91 && a <= 96) {
-            password.push(String.fromCharCode(a));
-            inputChecked++;
-          } else if (a >= 123 && a <= 126) {
-            password.push(String.fromCharCode(a));
-            inputChecked++;
-          }
-        } else {
-          if (a >= 32 && a <= 47) {
-            password.pop(String.fromCharCode(a));
-            inputChecked--;
-          } else if (a >= 58 && a <= 64) {
-            password.pop(String.fromCharCode(a));
-            inputChecked--;
-          } else if (a >= 91 && a <= 96) {
-            password.pop(String.fromCharCode(a));
-            inputChecked--;
-          } else if (a >= 123 && a <= 126) {
-            password.pop(String.fromCharCode(a));
-            inputChecked--;
-          }
-        }
-      }
-    }
-    strengthPassword(inputChecked);
-
-    takePassword(password);
-  });
-});
-
-// Generowanie hasła
-function takePassword(password) {
-  button.addEventListener('click', e => {
-    // Z ilu znaków składa się hasło
-    let num = charLng.innerHTML;
-    // Zamiana tablicy na string, aby użyć charAt. Bez join() dokłada przecinki
-    let str = password.join('').toString();
-    let arr = [];
-
-    console.log(num)
-
-    if (num === '') {
-      alert('Zaznacz długość hasła');
-    }
-    // Losowanie znaków
-    for (let i = 0; i < num; i++) {
-      const res = Math.floor(Math.random() * password.length);
-      arr.push(str.charAt(res));
-    }
-    // Wyświetlenie hasła. Bez join() dodaje przecinki
-    resCopy.innerHTML = arr.join('');
-  });
-}
-
-function onDragSuw(e) {
-  // console.log(e.target.value)
-  let suwVal = e.target.value;
-  charLng.innerText = suwVal;
-}
-
-suw.addEventListener('click', onDragSuw);
-
-// Generowanie hasła
-
-// button.addEventListener('click', e=>{
-//   let randomPassword = '';
-
-// function takePassword(password) {
-//   console.log(password)
-//   for (let i=0; i<=password.length; i++){
-//     let res = Math.floor(Math.random()*suwVal.length);
-// randomPassword.concat(res);
-//   }
-// }
-
-//   console.log(pass)
-
-//   resCopy.innerHTML = randomPassword;
-
-// })
 
 // console.log(password);
 
