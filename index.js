b9e1cf578be946952d85880bc72e6a93f6cbec5d
Agnieszka Magiera <agnieszka_magiera@wp.pl>
2024-02-05 17:43:27 +0100

    Dodawanie wielkich i malych liter - do poprawy

--- index.js
+++ index.js
@@ -166,112 +166,42 @@ const button = document.querySelector('button');
 const strBar = document.querySelector('.str-bar');
 const bar = document.querySelectorAll('.bar');
 const input = document.querySelectorAll('#input-check');
+
 let inputChecked = 0;
-let password = [];
+let password = '';
 
-// console.log(password);
 
-// // Dodawanie wielkich liter do hasła
-// function addUpCase(e) {
-//   // const uc1= password.toString().match(/[A-Z]/g)
-//   const uc = [];
+// Dodawanie wielkich liter do hasła
+input.forEach(i=>{
+    i.addEventListener('click', e=>{
+        if(i.className==='upCase' && e.target.checked){
+            let upCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+            password = password.concat('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+            inputChecked++;
+            // e.target.checked ? inputChecked++ : inputChecked--;
+        }else {
+            
+            password= password.match(/[^A-Z]/);
+            inputChecked--;
+        }
+        
+        if(i.className==='lowCase' && e.target.checked){
+            password = password.concat('abcdefghijklmnopqrstuvwxyz');
+            // e.target.checked ? inputChecked++ : inputChecked--;
+        }else {
+            
+            password= password.match(/[^a-z]/);
+            inputChecked--;
+        }
+        console.log(password, inputChecked)
+    })
+   
+})
 
-//   for (let i = 65; i <= 90; i++) {
-//     uc.push(String.fromCharCode(i));
-//   }
-//   const ucChecked = e.target.checked;
+// Dodawanie małych liter do hasła
 
-//   if (ucChecked) {
-//     password.push(uc);
-//     inputChecked++;
-//     console.log(inputChecked);
-//   }else {
-//     inputChecked--;
-//     console.log(inputChecked);
-//   }
-//   // return password;
-// }
-// // Dodawanie małych liter do hasła
-// function addLowCase(e) {
-//   // const lc1= password.toString().match(/[a-z]/g)
-//   const lc = [];
 
-//   for (let i = 97; i <= 122; i++) {
-//     lc.push(String.fromCharCode(i));
-//   }
-//   const lcChecked = e.target.checked;
-
-//   if (lcChecked) {
-//     password.push(lc);
-//     inputChecked++;
-//     console.log(inputChecked);
-//   }else {
-//     inputChecked--;
-//     console.log(inputChecked);
-//   }
-//   //   resCopy.innerHTML = password.join('');
-// }
-
-// // Dodawanie cyfr do hasła
-// function addNumb(e) {
-//   // const num1= password.toString().match(/[0-9]/g)
-//   const num = [];
-
-//   for (let i = 48; i <= 57; i++) {
-//     num.push(String.fromCharCode(i));
-//   }
-
-//   const numChecked = e.target.checked;
-
-//   if (numChecked) {
-//     password.push(num);
-//     inputChecked++;
-//     console.log(inputChecked);
-//   }else {
-//     inputChecked--;
-//     console.log(inputChecked);
-//   }
-// }
-
-// // Dodawanie znaków do hasła
-// function addChar(e) {
-//   // const char1 = password.toString().match(/[^0-9][^a-z][^A-Z]/g);
-//   const char = [];
-
-//   for (let i = 32; i <= 126; i++) {
-//     if (i >= 32 && i <= 47) {
-//       char.push(String.fromCharCode(i));
-//     } else if (i >= 58 && i <= 64) {
-//       char.push(String.fromCharCode(i));
-//     } else if (i >= 91 && i <= 96) {
-//       char.push(String.fromCharCode(i));
-//     } else if (i >= 123 && i <= 126) {
-//       char.push(String.fromCharCode(i));
-//     }
-//   }
-
-//   const charChecked = e.target.checked;
-
-//   if (charChecked) {
-//     password.push(char);
-//     inputChecked++;
-//     console.log(inputChecked);
-//   } else {
-//     inputChecked--;
-//     console.log(inputChecked);
-//   }
-// }
-
-// // zaznaczanie mocy hasła
-// bar.forEach(b=>{
-//   if(inputChecked<=2){
-// strBar.innerHTML='słaba';
-//   }else if(inputChecked===3){
-//     strBar.innerHTML='średnia'
-//   }else{
-//     strBar.innerHTML='mocna'
-//   }
-// })
+// zaznaczanie mocy hasła
 
 // // Generowanie hasła
 // button.onclick = function takePassword() {
@@ -279,18 +209,14 @@ let password = [];
 //   resCopy.innerHTML = password;
 // };
 
-// upCase.addEventListener('input', addUpCase);
-// lowCase.addEventListener('input', addLowCase);
-// numbers.addEventListener('input', addNumb);
-// chars.addEventListener('input', addChar);
-// button.addEventListener('click', takePassword);
 
-// function onDragSuw(e) {
-//   // console.log(e.target.value)
-//   let suwVal = e.target.value;
-//   charLng.innerText = suwVal;
-// }
 
-// suw.addEventListener('click', onDragSuw);
+function onDragSuw(e) {
+  // console.log(e.target.value)
+  let suwVal = e.target.value;
+  charLng.innerText = suwVal;
+}
 
-// console.log(suw.value);
+suw.addEventListener('click', onDragSuw);
+
+console.log(suw.value);
